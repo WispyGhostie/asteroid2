cb4901f663b222760009dfd0926f564854ff3cef
Wispy <wispyghostie@proton.me>
2025-04-23 16:18:09 -0400

    Ship was deployed

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     clock = pygame.time.Clock()
     dt = 0
@@ -26,9 +29,13 @@ def main():
         #otherwise you would call pygame.Surface.fill() to create the screen
         #with particular inputs
         
+        player.draw(screen)
+
+
         pygame.display.flip()
         
         
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
     
