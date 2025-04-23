01d124a4abfb84fbcc417159388a1a4edd76acbd
Wispy <wispyghostie@proton.me>
2025-04-23 15:30:30 -0400

    newfix

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def main():
 
     pygame.init()
 
-
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
 
 
@@ -21,10 +22,18 @@ def main():
         
         
         screen.fill("black")
-        
-        
+        #You call screen here because it has the width and height
+        #otherwise you would call pygame.Surface.fill() to create the screen
+        #with particular inputs
         
         pygame.display.flip()
+        
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
+    #The if __name__ == "__main__": block checks whether the script is being run directly.
+    #If it is, the block of code beneath it will execute.
+    #If the script is imported, the block will not run.
